73d44a4277199ce9c540238a39ec5adf207b6677
programmingroup <luisgx14@gmail.com>
2025-01-09 21:14:24 -0500

    feat: :sparkles: Modificated model

--- shopping-item-list-service/Models/ShoppingItem.cs
+++ shopping-item-list-service/Models/ShoppingItem.cs
@@ -4,9 +4,7 @@ namespace ShoppingItemService.Models
     {
         public int Id { get; set; }
         public required int ProductId { get; set; }
-        public required string ProductName { get; set; }
         public required int Quantity { get; set; }
-        public required decimal Price { get; set; }
         public required decimal Subtotal { get; set; }
     }
 
